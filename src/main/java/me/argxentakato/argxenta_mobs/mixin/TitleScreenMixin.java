9a597953b4065fb78509680b83038f7417f6fd71
Unuaviro F. Arĝentakato <unuaviro@argxentakato.me>
2024-11-23 15:44:27 +0900

    Initialization project

--- src/main/java/me/argxentakato/argxenta_mobs/mixin/TitleScreenMixin.java
+++ src/main/java/me/argxentakato/argxenta_mobs/mixin/TitleScreenMixin.java
@@ -1,16 +1,17 @@
-package com.example.example_mod.mixin;
+package me.argxentakato.argxenta_mobs.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import me.argxentakato.argxenta_mobs.ArgxentaMobs;
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ArgxentaMobs.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
